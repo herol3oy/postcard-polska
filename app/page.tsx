9d7ea821e830596c51fb151dadda3962da43ba60
herol3oy <hamedsedighi@gmail.com>
2025-06-21 19:47:13 +0200

    config stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return "hi";
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { canceled: boolean };
+}) {
+  const { canceled } = await searchParams;
+
+  if (canceled) {
+    console.log(
+      "Order canceled -- continue to shop around and checkout when you’re ready."
+    );
+  }
+  return (
+    <form action="/api/checkout_sessions" method="POST">
+      <section>
+        <button type="submit" role="link">
+          Checkout
+        </button>
+      </section>
+    </form>
+  );
 }
