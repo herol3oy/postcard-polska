fa3a5cb33d6db1b5c7fd1d978a0dccc833bf7107
herol3oy <hamedsedighi@gmail.com>
2025-06-24 18:58:20 +0200

    update image graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,25 @@ import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Postcard Polska | Buy the most beautiful Warsaw postcard',
-  description: 'Buy the most beautiful Warsaw postcard featuring the iconic Warsaw Mermaid. Printed on premium matte stock with love from Poland.',
-}
+  title: "Postcard Polska | Buy the most beautiful Warsaw postcard",
+  description:
+    "Buy the most beautiful Warsaw postcard featuring the iconic Warsaw Mermaid. Printed on premium matte stock with love from Poland.",
+  openGraph: {
+    title: "Warszawska Syrenka Postcard",
+    description: "Sealed with love 💌",
+    images: [
+      {
+        url: "https://postcardpolska.pl/warszawska_syrenka_postcard_front.JPG",
+        width: 500,
+        height: 698,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: ["https://postcardpolska.pl/warszawska_syrenka_postcard_front.JPG"],
+  },
+};
 
 export default function RootLayout({
   children,
