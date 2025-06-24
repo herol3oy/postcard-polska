2c1a84a42d49797aad345cfd36c4fb223ca11d94
herol3oy <hamedsedighi@gmail.com>
2025-06-24 17:51:21 +0200

    add promotion code

--- app/api/checkout_sessions/route.ts
+++ app/api/checkout_sessions/route.ts
@@ -19,6 +19,7 @@ export async function POST() {
         },
       ],
       mode: 'payment',
+      allow_promotion_codes: true,
       success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/?canceled=true`,
     });
